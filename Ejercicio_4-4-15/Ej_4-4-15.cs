25fefd3882d6edab228debc5208829817b52a282
a14manuelmb <a14manuelmb@iessanclemente.net>
2015-04-26 04:59:44 +0200

    Código métodos 15

--- Ejercicio_4-4-15/Ej_4-4-15.cs
+++ Ejercicio_4-4-15/Ej_4-4-15.cs
@@ -20,17 +20,72 @@ namespace Ejercicio_4_4_15
         public string colorOjos15;
         public string colorPelo15;
 
-
+        private bool lentillaValida;
+        private bool pesoValido;
+        
         public void PonerLentillas15(string colorLentillas)
-        { 
+        {
+            lentillaValida = false;
+            lentillaValida = ComprobarColor15(colorLentillas);
+
+            if (lentillaValida)
+            {
+                this.colorOjos15 = colorLentillas;
+            }
         }
 
         public void Engordar15(int cantidad)
         {
+            double novoPeso = this.peso15 + cantidad;
+            pesoValido = ComprobarPeso15(novoPeso);
+
+            if (!pesoValido || cantidad < 0)
+            {
+                Console.WriteLine("Cantidade incorrecta");
+            }
+            else
+            {
+                Console.WriteLine("Cambiando o peso en {0} kg", cantidad);
+                this.peso15 = (int)novoPeso;
+            }
         }
 
         public void Adelgazar15(double cantidad)
         {
+            double novoPeso = this.peso15 - cantidad;
+            pesoValido = ComprobarPeso15(novoPeso);
+
+            if (!pesoValido || cantidad < 0)
+            {
+                Console.WriteLine("Cantidade incorrecta");
+            }
+            else
+            {
+                Console.WriteLine("Cambiando o peso en {0} kg", cantidad);
+                this.peso15 = (int)novoPeso;
+            }
+        }
+
+        private bool ComprobarColor15(string color)
+        {
+            if (color.Equals("azul") || color.Equals("marrón") || color.Equals("verde"))
+            {
+                lentillaValida = true;
+            }
+            return lentillaValida;
+        }
+
+        private bool ComprobarPeso15(double novoPeso)
+        {
+            if (novoPeso < 40 || novoPeso > 180)
+            {
+                pesoValido = false;
+            }
+            else
+            {
+                pesoValido = true;
+            }
+            return pesoValido;
         }
     }
 }
