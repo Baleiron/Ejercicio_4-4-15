4808aba0fe002fc84a6b2eaff41f678a16b3413a
a14manuelmb <a14manuelmb@iessanclemente.net>
2015-04-26 04:57:46 +0200

    Inicializacion15

---
+++ Ejercicio_4-4-15/Ej_4-4-15.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ejercicio_4_4_15
+{
+    public class Persona15
+    {
+        public Persona15(int altura, int peso, string colorOjos, string colorPelo)
+        {
+            this.altura15 = altura;
+            this.colorOjos15 = colorOjos;
+            this.colorPelo15 = colorPelo;
+            this.peso15 = peso;
+        }
+
+        public int peso15;
+        public int altura15;
+        public string colorOjos15;
+        public string colorPelo15;
+
+
+        public void PonerLentillas15(string colorLentillas)
+        { 
+        }
+
+        public void Engordar15(int cantidad)
+        {
+        }
+
+        public void Adelgazar15(double cantidad)
+        {
+        }
+    }
+}
